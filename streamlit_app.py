2fcaa0d4969992ea2c5f4e319e6629ffc5395762
jaelee90 <jaelee90@gmail.com>
2023-02-03 14:47:29 -0500

    Update streamlit_app.py
    syntax error

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,7 +35,7 @@ try:
     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
     streamlit.dataframe(fruityvice_normalized)
     
-except error as e:
+except error as e
 
 
 streamlit.stop()
